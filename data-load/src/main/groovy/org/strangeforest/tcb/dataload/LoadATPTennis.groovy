337561372948b6033321e13dbaf609b20fd29306
Romain Ramier <romain@sportsnconnect.com>
2026-09-18 16:22:01 +0200

    fix

--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennis.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennis.groovy
@@ -2,24 +2,35 @@ package org.strangeforest.tcb.dataload
 
 import com.google.common.base.*
 
-println 'Loading Tennis Data'
+println 'Chargement des données tennistiques'
 def stopwatch = Stopwatch.createStarted()
 
 def sqlPool = new SqlPool()
 def loader = new ATPTennisLoader()
 
 loader.loadPlayers(new PlayerLoader(sqlPool))
+sqlPool.withSql { sql -> loader.loadAdditionalPlayerData(sql) }
 
 loader.loadRankings(new RankingLoader(sqlPool))
+sqlPool.withSql { sql -> loader.loadAdditionalRankingData(sql) }
+sqlPool.withSql { sql -> MissingRankingsLoader.loadRankings(sql) }
+LoadNewRankings.loadRankings(sqlPool)
 
 loader.loadMatches(new MatchLoader(sqlPool))
+sqlPool.withSql { sql -> loader.loadAdditionalTournamentData(sql) }
+LoadNewTournaments.loadTournaments(sqlPool)
 
 sqlPool.withSql { sql -> loader.vacuum(sql) }
+
+if (LoadParams.getBooleanProperty(LoadParams.FULL_LOAD_PROPERTY, LoadParams.FULL_LOAD_DEFAULT)) {
+	sqlPool.withSql { sql -> loader.correctDataFull(sql) }
+}
 
 EloRatingsRunner.computeEloRatings(LoadParams.getBooleanProperty(LoadParams.FULL_LOAD_PROPERTY, LoadParams.FULL_LOAD_DEFAULT))
 
 sqlPool.withSql { sql -> loader.vacuum(sql) }
 
+sqlPool.withSql { sql -> loader.correctData(sql) }
 sqlPool.withSql { sql -> loader.refreshMaterializedViews(sql) }
 
 sqlPool.withSql { sql -> loader.vacuum(sql) }
